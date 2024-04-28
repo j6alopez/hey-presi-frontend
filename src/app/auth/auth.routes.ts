e39503930b1aa530b1b6ad1fcf65da4ba0165eb7
jose <j6alopez@gmail.com>
2024-04-28 11:55:11 +0200

    feat: nav bar layout wip

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
-// Add the missing import statements above
-
 
 export const AUTH_ROUTES: Routes = [
   {
@@ -11,7 +8,6 @@ export const AUTH_ROUTES: Routes = [
     component: LayoutPageComponent,
     children: [
       { path: 'login', component: LoginPageComponent },
-      { path: 'register', component: RegisterPageComponent },
       { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: '**', redirectTo: 'login' },
     ]
--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TopBarComponent } from '../../shared/components/top-bar/top-bar.component';
+import { SideBarComponent } from '../../shared/components/side-bar/side-bar.component';
 
 
 @Component({
   selector: 'home-page',
   standalone: true,
   imports: [
+    SideBarComponent,
+    TopBarComponent,
     RouterModule
   ],
   templateUrl: './home-page.component.html',
