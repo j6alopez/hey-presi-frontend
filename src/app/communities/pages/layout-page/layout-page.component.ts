4c66024f2d11071a52a0b11c6af4d88f34a67fc1
jose <j6alopez@gmail.com>
2024-05-22 10:12:49 +0200

    fix: standarize titles

--- src/app/communities/pages/layout-page/layout-page.component.ts
+++ src/app/communities/pages/layout-page/layout-page.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideBarComponent } from '../../../shared/components/side-bar/side-bar.component';
+import { TopBarComponent } from '../../../shared/components/top-bar/top-bar.component';
+import { MenuItem } from '../../../shared/interfaces/menu-item.interface';
 
 @Component({
   standalone: true,
   imports: [
-    RouterModule
+    RouterModule,
+    SideBarComponent,
+    TopBarComponent
   ],
   templateUrl: './layout-page.component.html',
   styleUrl: './layout-page.component.scss'
 })
 export class LayoutPageComponent {
+  public menuItems: MenuItem[] = [
+    { name: 'Comunidades', route: '/community/main' },
+    { name: 'Cuentas', route: '#' },
+    { name: 'Incidencias', route: '/tasks' },
+    { name: 'Servicios', route: '#' },
+  ];
 
 }
