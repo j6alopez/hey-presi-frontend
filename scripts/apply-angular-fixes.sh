a7b4c133997070a47b9a0c08e4c1c8429dfd36f3
jose <j6alopez@gmail.com>
2024-07-23 10:54:45 +0200

    fix: apply-angular fixes

--- scripts/apply-angular-fixes.sh
+++ scripts/apply-angular-fixes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ng update \
-  @angular-devkit/build-angular@^18 \
   @angular/cli@^18 \
   @angular/compiler-cli@^18 \
   @angular/localize@^18 \
+  @angular-devkit/build-angular@^18 \
 && npm install
