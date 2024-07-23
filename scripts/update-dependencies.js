139ac6ed38653a23273b63bd48f0ffe85a7d42b7
jose <j6alopez@gmail.com>
2024-07-23 15:53:11 +0200

    feat

--- scripts/update-dependencies.js
+++ scripts/update-dependencies.js
@@ -1,4 +1,4 @@
-const { exec } = require('child_process');
+import { exec } from 'child_process';
 
 const runCommand = (command) => {
   return new Promise((resolve, reject) => {
@@ -16,13 +16,8 @@ const main = async () => {
   try {
     console.log('Updating Angular packages...');
 
-    const updateCommand = `
-      ng update \
-        @angular/cli@^18 \
-        @angular/compiler-cli@^18 \
-        @angular/localize@^18 \
-        @angular-devkit/build-angular@^18
-    `;
+    // Use a single line for the ng update command
+    const updateCommand = 'ng update @angular/cli@^18 @angular/compiler-cli@^18 @angular/localize@^18 @angular-devkit/build-angular@^18';
     const updateOutput = await runCommand(updateCommand);
     console.log(updateOutput);
 
