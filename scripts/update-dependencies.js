77b34330c1cdec63603587b7f279b7f27c9675f3
jose <j6alopez@gmail.com>
2024-07-23 15:50:43 +0200

    feat

---
+++ scripts/update-dependencies.js
@@ -0,0 +1,40 @@
+const { exec } = require('child_process');
+
+const runCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error: ${stderr}`);
+        return;
+      }
+      resolve(stdout);
+    });
+  });
+};
+
+const main = async () => {
+  try {
+    console.log('Updating Angular packages...');
+
+    const updateCommand = `
+      ng update \
+        @angular/cli@^18 \
+        @angular/compiler-cli@^18 \
+        @angular/localize@^18 \
+        @angular-devkit/build-angular@^18
+    `;
+    const updateOutput = await runCommand(updateCommand);
+    console.log(updateOutput);
+
+    console.log('Installing packages...');
+
+    const installOutput = await runCommand('npm install');
+    console.log(installOutput);
+
+    console.log('All done!');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
