5ac634747e94c1c0ef540b327b6289ee8e3d5366
jose <j6alopez@gmail.com>
2024-07-23 14:53:27 +0200

    fix

--- scripts/apply-angular-fixes.js
+++ scripts/apply-angular-fixes.js
@@ -1,8 +1,42 @@
 #!/usr/bin/env node
 
-ng update \
-  @angular/cli@^18 \
-  @angular/compiler-cli@^18 \
-  @angular/localize@^18 \
-  @angular-devkit/build-angular@^18 \
-&& npm install
+const { exec } = require('child_process');
+
+const runCommand = (command) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(`Error: ${stderr}`);
+        return;
+      }
+      resolve(stdout);
+    });
+  });
+};
+
+const main = async () => {
+  try {
+    console.log('Updating Angular packages...');
+
+    const updateCommand = `
+      ng update \
+        @angular/cli@^18 \
+        @angular/compiler-cli@^18 \
+        @angular/localize@^18 \
+        @angular-devkit/build-angular@^18
+    `;
+    const updateOutput = await runCommand(updateCommand);
+    console.log(updateOutput);
+
+    console.log('Installing packages...');
+    
+    const installOutput = await runCommand('npm install');
+    console.log(installOutput);
+
+    console.log('All done!');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+main();
